fc1ff8fb5ae65ba3a9365da2f609c5d691641b07
Hanna <hannaseraphim@gmail.com>
2025-11-14 19:00:00 -0400

    Fixed table from 'courses' to 'associated'

--- src/modules/AssociatedModel.ts
+++ src/modules/AssociatedModel.ts
@@ -1,8 +1,12 @@
 import type { Connection } from "mysql2/promise";
 import { BaseModel } from "../core/BaseModel.js";
+import { UserProfile } from "./UserProfile.js";
+import connection from "../config/connection.js";
+
+const userProfile = new UserProfile(connection);
 
 export class AssociatedModel extends BaseModel {
-    protected table = 'courses';
+    protected table = 'associated';
     protected fields = ["id_user", "id_profile"];
     protected requiredFields: string[];
 
@@ -10,4 +14,19 @@ export class AssociatedModel extends BaseModel {
         super(db);
         this.requiredFields = [...this.fields]
     }
+
+    // Query item by id
+    async findAssociated(id: number) {
+        const query = `SELECT * FROM \`${this.table}\` WHERE id_user = ?`;
+        const [rows] = await this.db.execute(query, [id]);
+            
+        const roles: string[] = [];
+
+        for (const row of rows as any[]) {
+            const profile = await userProfile.findById(row.id_profile);
+            roles.push(profile.name);
+        }
+
+        return roles;
+  }
 }
