0db5c8978f5f4e28eea1a0294bdd3a74cd11b35a
Hanna <hannaseraphim@gmail.com>
2025-11-14 18:53:10 -0400

    User routes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,2 +1,31 @@
 import connection from "../config/connection.js";
 import { UserModel } from "../modules/UserModel.js";
+import type { Request, Response } from "express";
+
+const userModel = new UserModel(connection);
+
+export const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const users = await userModel.findAll();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }   
+}
+
+export const getUserById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const user = await userModel.findById(Number(id));
+        if(!user) {
+            res.status(404).json({ Error: "User not found" });
+            return;
+        }
+        
+        res.status(200).json(user);
+    } catch (err){
+        console.log(err);
+        res.status(500).json({ Error: "Internal Server Error" });   
+    }
+}
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { getUserById, getAllUsers } from "../controllers/user.controller.js";
 
 const router = Router();
+
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
 
 export default router;
